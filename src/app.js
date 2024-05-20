01bf4f87b8a9bd1e9fc4883e240e234888fd7423
aakashstha1 <aakash.cerestha90@gmail.com>
2024-05-20 21:02:01 +0545

    first commit

--- src/app.js
+++ src/app.js
@@ -5,10 +5,9 @@ require("./db/conn"); //Connect to DB
 const app = express(); //Instance of an Express application
 const port = process.env.PORT || 8000;
 
-// const static_path = path.join(__dirname, "../public");
-//can be use if the static file is served from public folder i.e index.html
+const static_path = path.join(__dirname, "../public");
 
-const templates_path = path.join(__dirname, "../templates");
+const views_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials");
 
 //Middleware
@@ -25,17 +24,20 @@ app.use(
   express.static(path.join(__dirname, "../node_modules/jquery/dist"))
 );
 
-//Since I am not using index.html so i don't need this
-// app.use(express.static(static_path));
+app.use(express.static(static_path));
 //This middleware serves static files from the public directory. When a request matches a file in this directory, the file is served automatically.
 
 app.set("view engine", "hbs"); //Setting handlebars as view engine
-app.set("views", templates_path); //Tell express the path of views
+app.set("views", views_path); //Tell express the path of views
 hbs.registerPartials(partials_path); //Register particals which are reusable components in hbs
 
 //Routing
 app.get("/", (req, res) => {
   res.render("index"); //When a GET request is made to this URL, the server responds with "Hello!"
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 //Create Server
